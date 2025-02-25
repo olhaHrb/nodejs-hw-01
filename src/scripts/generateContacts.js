5e001cb529be5f0de44872021db3dd95bf96704b
olhaHrb <150930419+olhaHrb@users.noreply.github.com>
2025-02-25 17:38:48 +0200

    generateContacts

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,12 @@
-const generateContacts = async (number) => {};
+import { faker } from '@faker-js/faker';
+import { createFakeContact } from '../utils/createFakeContact';
+import { writeContacts } from '../utils/writeContacts';
+
+const generateContacts = async (number) => {
+  const contacts = faker.helpers.multiple(createFakeContact, {
+    count: number,
+  });
+  writeContacts(contacts);
+};
 
 generateContacts(5);
--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -1,3 +1,10 @@
+import fs from 'node:fs/promises';
 import { PATH_DB } from '../constants/contacts.js';
 
-export const writeContacts = async (updatedContacts) => {};
+export const writeContacts = async (updatedContacts) => {
+  try {
+    await fs.writeFile(PATH_DB, updatedContacts);
+  } catch (err) {
+    console.error(err);
+  }
+};
