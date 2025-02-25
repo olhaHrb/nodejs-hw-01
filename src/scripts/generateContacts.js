e2b4f4541aabfc8043ea33a2a20ae62350e3bedb
olhaHrb <150930419+olhaHrb@users.noreply.github.com>
2025-02-25 21:58:48 +0200

    read contacts

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -3,12 +3,15 @@ import { createFakeContact } from '../utils/createFakeContact.js';
 import { writeContacts } from '../utils/writeContacts.js';
 import { readContacts } from '../utils/readContacts.js';
 
+//const contacts = readContacts();
 readContacts();
 
 const generateContacts = async (number) => {
   const newContacts = faker.helpers.multiple(createFakeContact, {
     count: number,
   });
+
+  //console.log(contacts);
 
   writeContacts(newContacts);
 };
--- src/utils/readContacts.js
+++ src/utils/readContacts.js
@@ -1,13 +1,15 @@
 import { PATH_DB } from '../constants/contacts.js';
 import fs from 'node:fs/promises';
 
-export const readContacts = async () => {
-  try {
-    const data = await fs.readFile(PATH_DB, { encoding: 'utf8' });
+export const readContacts = () => {
+  fs.readFile(PATH_DB, { encoding: 'utf8' })
+    .then((data) => {
+      //const data = await fs.readFile(PATH_DB, { encoding: 'utf8' });
 
-    //return data;
-    console.log('вміст:', data);
-  } catch (err) {
-    console.error(err);
-  }
+      console.log(data);
+      //return data;
+    })
+    .catch((err) => {
+      console.error(err);
+    });
 };
