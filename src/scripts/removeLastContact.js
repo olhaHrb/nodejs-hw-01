1c83edfec17948ef1b5d599aebfc7bc0c948324f
olhaHrb <150930419+olhaHrb@users.noreply.github.com>
2025-02-26 13:18:52 +0200

    removeLastContact

--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -1,3 +1,13 @@
-export const removeLastContact = async () => {};
+import { writeContacts } from '../utils/writeContacts.js';
+import { readContacts } from '../utils/readContacts.js';
+
+export const removeLastContact = async () => {
+  const contacts = await readContacts();
+  const arrContacts = JSON.parse(contacts);
+
+  const newContacts = arrContacts.slice(0, arrContacts.length - 1);
+
+  writeContacts(newContacts);
+};
 
 removeLastContact();
